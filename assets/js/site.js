1e3739c7516c8965e3a3d613f54cb9e94b5132d6
willy0483 <145011279+willy0483@users.noreply.github.com>
2024-04-16 09:42:10 +0200

    Initial commit

---
+++ assets/js/site.js
@@ -0,0 +1,13 @@
+// write cool JS here!!
+const myInfo = 'en variabel';
+const myBooks = ["The Catcher in the Rye",
+    "To Kill a Mockingbird",
+    "1984",
+    "Pride and Prejudice",
+    "The Great Gatsby",
+    "The Hobbit",
+    "Harry Potter and the Sorcerer's Stone",
+    "The Lord of the Rings",
+    "The Da Vinci Code",
+    "The Hunger Games"]
+
